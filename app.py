871efe1df96c8ea085504bb1191f73e905cd4c17
IshaanOhri <ishaan99ohri@gmail.com>
2020-07-18 14:32:58 +0530

    Free hand drawing

--- app.py
+++ app.py
@@ -1,21 +1,45 @@
 import sys
-from PyQt5 import QtCore
+from PyQt5 import QtGui, QtCore
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtGui import QPainter, QBrush, QPen, QColor
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
         
-        self.FullWindow()
+        self.start, self.end = QtCore.QPoint(), QtCore.QPoint()
+        self.fullWindow()
 
-    
-    def FullWindow(self):
+    def fullWindow(self):
+        self.setWindowTitle('Capture')
         flags = QtCore.Qt.WindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.Dialog)
         self.setWindowFlags(flags)
         self.showMaximized()
-        self.setStyleSheet("background-color: rgba(255,255,255,0.2); border: 3px solid rgb(16, 229, 125);")
+        self.setStyleSheet("background-color: rgba(255,255,255,0.0); border: 3px solid rgb(16, 229, 125);")
         self.show()
+
+    def mousePressEvent(self, session):
+        self.start = self.end = session.pos()
+        self.update()
+        return super().mousePressEvent(session)
+
+    def mouseReleaseEvent(self, session):
+        self.end = session.pos()
+        self.update()
+        return super().mouseReleaseEvent(session)
+
+    def mouseMoveEvent(self, session):
+        self.end = session.pos()
+        self.update()
+        return super().mousePressEvent(session)
+
+    def paintEvent(self, e):
+        painter = QPainter(self)
+        painter.setPen(QPen(Qt.white, 1, Qt.SolidLine))
+        painter.setBrush(QBrush(QColor.fromRgbF(255,255,255,0.2), Qt.SolidPattern))
+        painter.drawRect(QtCore.QRect(self.start, self.end))
+
 
 
 if __name__ == "__main__":
